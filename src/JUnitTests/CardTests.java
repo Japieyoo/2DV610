17fc2b4d1ad55b9a244fd28c8a76d3b74be05fd6
Japieyoo <japieyoo@gmail.com>
2018-11-14 14:31:19 +0100

    GREEN

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -1,19 +1,21 @@
 package JUnitTests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 import model.Card;
 
 class CardTests {
 
-    @Test
-    public void ColorEnum_CreateEnum_ReturnFourItems() {
-        int actual = Card.Color.values().length;
-        int expected = 4;
+	@Test
+	public void ColorEnum_CreateEnum_ReturnFourItems() {
+		int actual = Card.Color.values().length;
+		int expected = 4;
 
-        assertEquals(expected, actual);
-    }
+		assertEquals(expected, actual);
+	}
 
 }
