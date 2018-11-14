89e8c08004e8b6185c347c716fbcfc72a5fff6d7
Japieyoo <japieyoo@gmail.com>
2018-11-14 15:00:02 +0100

    GREEN

--- src/model/Card.java
+++ src/model/Card.java
@@ -7,6 +7,16 @@ public class Card {
 	}
 
 	public enum CardValue {
+		A(1), B(2), C(3), D(4), E(5), F(6), G(7), H(8), I(9), J(0), K(10), L(11), M(12);
 
+		private int numVal;
+
+		CardValue(int numVal) {
+			this.numVal = numVal;
+		}
+
+		public int getNumVal() {
+			return numVal;
+		}
 	}
 }
