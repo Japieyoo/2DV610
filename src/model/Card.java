ef96fca6bfa1f85728b0dd0e13422b01a927d178
Japieyoo <japieyoo@gmail.com>
2018-11-14 15:58:07 +0100

    GREEN

--- src/model/Card.java
+++ src/model/Card.java
@@ -7,7 +7,7 @@ public class Card {
 	}
 
 	public enum CardValue {
-		A(1), B(2), C(3), D(4), E(5), F(6), G(7), H(8), I(9), J(0), K(10), L(11), M(12);
+		ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(0), ELEVEN(10), TWELVE(11), THIRTEEN(12);
 
 		private int numVal;
 
