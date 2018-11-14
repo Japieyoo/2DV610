ce3fd949367f7ff92d9c1fe54ac9a7de0e1f8655
Japieyoo <japieyoo@gmail.com>
2018-11-14 15:57:08 +0100

    RED

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -2,12 +2,13 @@ package JUnitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import model.Card;
+import model.Card.CardValue;
 import model.Card.Color;
 
 class CardTests {
@@ -47,7 +48,7 @@ class CardTests {
 
 		assertTrue(actual);
 	}
-	
+
 	@Test
 	public void CardValueEnum_CheckThatItContainsThirteenValues_ReturnThirteen() {
 		int actual = Card.CardValue.values().length;
@@ -56,4 +57,62 @@ class CardTests {
 		assertEquals(expected, actual);
 	}
 
+	@Test
+	public void CardValueEnum_CheckThatItContainsThirteenCardValues_ReturnTrue() {
+		CardValue[] cardValueArray = Card.CardValue.values();
+		boolean aceExists = false;
+		boolean twoExists = false;
+		boolean threeExists = false;
+		boolean fourExists = false;
+		boolean fiveExists = false;
+		boolean sixExists = false;
+		boolean sevenExists = false;
+		boolean eightExists = false;
+		boolean nineExists = false;
+		boolean tenExists = false;
+		boolean elevenExists = false;
+		boolean twelveExists = false;
+		boolean thirteenExists = false;
+
+		boolean actual = false;
+
+		for (int i = 0; i < cardValueArray.length; i++) {
+			if (cardValueArray[i] == Card.CardValue.ACE) {
+				aceExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.TWO) {
+				twoExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.THREE) {
+				threeExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.FOUR) {
+				fourExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.FIVE) {
+				fiveExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.SIX) {
+				sixExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.SEVEN) {
+				sevenExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.EIGHT) {
+				eightExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.NINE) {
+				nineExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.TEN) {
+				tenExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.ELEVEN) {
+				elevenExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.TWELVE) {
+				twelveExists = true;
+			} else if (cardValueArray[i] == Card.CardValue.THIRTEEN) {
+				thirteenExists = true;
+			}
+		}
+
+		if (aceExists == true && twoExists == true && threeExists == true && fourExists == true && fiveExists == true
+				&& sixExists == true && sevenExists == true && eightExists == true && nineExists == true && tenExists == true
+				&& elevenExists == true && twelveExists == true && thirteenExists == true) {
+			actual = true;
+		}
+
+		assertTrue(actual);
+
+	}
 }
