1e803c9d3a1e824e48d29c676a99d8869fa9ee49
Japieyoo <japieyoo@gmail.com>
2018-11-14 14:28:13 +0100

    RED

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -1,5 +1,19 @@
 package JUnitTests;
 
-public class CardTests {
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import model.Card;
+
+class CardTests {
+
+    @Test
+    public void ColorEnum_CreateEnum_ReturnFourItems() {
+        int actual = Card.Color.values().length;
+        int expected = 4;
+
+        assertEquals(expected, actual);
+    }
 
 }
