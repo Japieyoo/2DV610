89bdc994259f758f94ad1dd6af1835f559dd9ac1
Japieyoo <japieyoo@gmail.com>
2018-11-14 16:06:58 +0100

    REFACTOR

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import model.Card;
 import model.Card.CardValue;
 import model.Card.Color;
@@ -23,30 +22,17 @@ class CardTests {
 
 	@Test
 	public void ColorEnum_CheckThatItContainsTheCorrectItems_ReturnTrue() {
+		int actual = 0;
+		int expected = 4;
 		Color[] colorArray = Card.Color.values();
-		boolean spadesExists = false;
-		boolean clubsExists = false;
-		boolean heartsExists = false;
-		boolean diamondsExists = false;
-		boolean actual = false;
-
+		Color[] correctArray = { Card.Color.Spades, Card.Color.Clubs, Card.Color.Hearts, Card.Color.Diamonds };
 		for (int i = 0; i < colorArray.length; i++) {
-			if (colorArray[i] == Color.Spades) {
-				spadesExists = true;
-			} else if (colorArray[i] == Color.Clubs) {
-				clubsExists = true;
-			} else if (colorArray[i] == Color.Hearts) {
-				heartsExists = true;
-			} else if (colorArray[i] == Color.Diamonds) {
-				diamondsExists = true;
+			if (Arrays.stream(correctArray).anyMatch(colorArray[i]::equals)) {
+				actual++;
 			}
 		}
 
-		if (spadesExists == true && clubsExists == true && heartsExists == true && diamondsExists == true) {
-			actual = true;
-		}
-
-		assertTrue(actual);
+		assertEquals(expected, actual);
 	}
 
 	@Test
@@ -58,61 +44,20 @@ class CardTests {
 	}
 
 	@Test
-	public void CardValueEnum_CheckThatItContainsThirteenCardValues_ReturnTrue() {
+	public void CardValueEnum_CheckThatItContainsTheACE_ReturnTrue() {
+		int actual = 0;
+		int expected = 13;
 		CardValue[] cardValueArray = Card.CardValue.values();
-		boolean aceExists = false;
-		boolean twoExists = false;
-		boolean threeExists = false;
-		boolean fourExists = false;
-		boolean fiveExists = false;
-		boolean sixExists = false;
-		boolean sevenExists = false;
-		boolean eightExists = false;
-		boolean nineExists = false;
-		boolean tenExists = false;
-		boolean elevenExists = false;
-		boolean twelveExists = false;
-		boolean thirteenExists = false;
-
-		boolean actual = false;
-
+		CardValue[] correctArray = { Card.CardValue.ACE, Card.CardValue.TWO, Card.CardValue.THREE, Card.CardValue.FOUR,
+				Card.CardValue.FIVE, Card.CardValue.SIX, Card.CardValue.SEVEN, Card.CardValue.EIGHT,
+				Card.CardValue.NINE, Card.CardValue.TEN, Card.CardValue.ELEVEN, Card.CardValue.TWELVE,
+				Card.CardValue.THIRTEEN };
 		for (int i = 0; i < cardValueArray.length; i++) {
-			if (cardValueArray[i] == Card.CardValue.ACE) {
-				aceExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.TWO) {
-				twoExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.THREE) {
-				threeExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.FOUR) {
-				fourExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.FIVE) {
-				fiveExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.SIX) {
-				sixExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.SEVEN) {
-				sevenExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.EIGHT) {
-				eightExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.NINE) {
-				nineExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.TEN) {
-				tenExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.ELEVEN) {
-				elevenExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.TWELVE) {
-				twelveExists = true;
-			} else if (cardValueArray[i] == Card.CardValue.THIRTEEN) {
-				thirteenExists = true;
+			if (Arrays.stream(correctArray).anyMatch(cardValueArray[i]::equals)) {
+				actual++;
 			}
 		}
 
-		if (aceExists == true && twoExists == true && threeExists == true && fourExists == true && fiveExists == true
-				&& sixExists == true && sevenExists == true && eightExists == true && nineExists == true && tenExists == true
-				&& elevenExists == true && twelveExists == true && thirteenExists == true) {
-			actual = true;
-		}
-
-		assertTrue(actual);
-
+		assertEquals(expected, actual);
 	}
 }
