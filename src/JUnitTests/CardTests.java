2139bc6f18f7c88a048b200aa0b6d1ecf264dd96
AntonFalt <anton.falt@hotmail.com>
2018-11-15 16:27:47 +0100

    REFACTOR

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -26,10 +26,10 @@ class CardTests {
 	public void ColorEnum_CheckThatItContainsTheCorrectItems_ReturnTrue() {
 		int actual = 0;
 		int expected = 4;
-		Color[] colorArray = Card.Color.values();
+		Color[] sut = Card.Color.values();
 		Color[] correctArray = { Card.Color.Spades, Card.Color.Clubs, Card.Color.Hearts, Card.Color.Diamonds };
-		for (int i = 0; i < colorArray.length; i++) {
-			if (Arrays.stream(correctArray).anyMatch(colorArray[i]::equals)) {
+		for (int i = 0; i < sut.length; i++) {
+			if (Arrays.stream(correctArray).anyMatch(sut[i]::equals)) {
 				actual++;
 			}
 		}
@@ -47,15 +47,15 @@ class CardTests {
 
 	@Test
 	public void CardValueEnum_CheckThatItContainsThirteenCardValues_ReturnTrue() {
+		CardValue [] sut = Card.CardValue.values();
 		int actual = 0;
 		int expected = 13;
-		CardValue[] cardValueArray = Card.CardValue.values();
 		CardValue[] correctArray = { Card.CardValue.ACE, Card.CardValue.TWO, Card.CardValue.THREE, Card.CardValue.FOUR,
 				Card.CardValue.FIVE, Card.CardValue.SIX, Card.CardValue.SEVEN, Card.CardValue.EIGHT,
 				Card.CardValue.NINE, Card.CardValue.TEN, Card.CardValue.ELEVEN, Card.CardValue.TWELVE,
 				Card.CardValue.THIRTEEN };
-		for (int i = 0; i < cardValueArray.length; i++) {
-			if (Arrays.stream(correctArray).anyMatch(cardValueArray[i]::equals)) {
+		for (int i = 0; i < sut.length; i++) {
+			if (Arrays.stream(correctArray).anyMatch(sut[i]::equals)) {
 				actual++;
 			}
 		}
@@ -67,9 +67,9 @@ class CardTests {
 	public void CardValuegetNumVal_CheckAllCardValuesAreCorrect_ReturnTrue() {
 		boolean actual = false;
 		int valueCheck = 1;
-		CardValue[] cardValueArray = Card.CardValue.values();
-		for (int i = 0; i < cardValueArray.length; i++) {
-			if (cardValueArray[i].getNumVal() == valueCheck++) {
+		CardValue[] sut = Card.CardValue.values();
+		for (int i = 0; i < sut.length; i++) {
+			if (sut[i].getNumVal() == valueCheck++) {
 				actual = true;
 			} else {
 				actual = false;
