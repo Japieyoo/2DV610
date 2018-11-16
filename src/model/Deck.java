0da48ba62dd88e5b16c00d632fb9f225c7dbd110
AntonFalt <anton.falt@hotmail.com>
2018-11-16 09:17:29 +0100

    GREEN

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -1,5 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Deck {
+	public ArrayList <Card > deck;
 	
+	public Deck() {
+		deck = new ArrayList<Card>();
+		Card c = new Card(Card.Color.Clubs, Card.CardValue.ACE);
+		for (int i = 0; i < 52; i++) {
+			deck.add(c);
+		}
+	}
 }
