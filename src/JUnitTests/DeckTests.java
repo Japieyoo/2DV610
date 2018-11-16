214b458d1b100e5323a6986bbe1d87eb3bcaf9ec
AntonFalt <anton.falt@hotmail.com>
2018-11-16 13:41:29 +0100

    RED

--- src/JUnitTests/DeckTests.java
+++ src/JUnitTests/DeckTests.java
@@ -1,10 +1,17 @@
 package JUnitTests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
+import model.Card;
 import model.Deck;
+import model.Card.CardValue;
+import model.Card.Color;
 
 class DeckTests {
 
@@ -16,4 +23,29 @@ class DeckTests {
 		int actual = sut.deck.size();
 		assertEquals(expected, actual);
 	}
+	@Test
+	public void Deck_CheckDifferentCards_Return52() {
+		Deck sut = new Deck();
+		int actual = 0;
+		int expected = 52;
+		int cardIndex = 0;
+		Card.Color[] colors = {Card.Color.Spades, Card.Color.Clubs, Card.Color.Hearts, Card.Color.Diamonds};
+		Card.CardValue[] values = {Card.CardValue.ACE, Card.CardValue.TWO, Card.CardValue.THREE, Card.CardValue.FOUR,
+		Card.CardValue.FIVE, Card.CardValue.SIX, Card.CardValue.SEVEN, Card.CardValue.EIGHT,
+		Card.CardValue.NINE, Card.CardValue.TEN, Card.CardValue.ELEVEN, Card.CardValue.TWELVE,
+		Card.CardValue.THIRTEEN};
+		for(int i = 0; i < colors.length; i++) {
+				for(int j = 0; j < values.length; j++) {
+					if (sut.deck.get(cardIndex).getCardValue() == values[j] && sut.deck.get(cardIndex).getColor() == colors[i]) {
+
+						actual++;
+						cardIndex++;
+					}
+				}
+			
+		}
+		assertEquals(expected, actual);
+	
+	}
+	
 }
