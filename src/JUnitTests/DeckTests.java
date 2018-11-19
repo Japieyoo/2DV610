dbbd434721b33b9410fb5ab4e3cdb4be5db5c0f6
AntonFalt <anton.falt@hotmail.com>
2018-11-19 15:19:08 +0100

    REFACTOR added stub to decktest

--- src/JUnitTests/DeckTests.java
+++ src/JUnitTests/DeckTests.java
@@ -27,19 +27,41 @@ class DeckTests {
 		}
 	}
 
+
 	@Test
 	public void Deck_CheckThatAllDifferentCardsExist_Return52() {
 
 		int actual = 0;
 		int expected = 52;
 		int cardIndex = 0;
-
+		ArrayList<CardStub> differentCardsStub = differentCardsStub();
 		Card.Color[] colors = Card.Color.values();
 		Card.CardValue[] values = Card.CardValue.values();
-		for (int i = 0; i < colors.length; i++) {
-			for (int j = 0; j < values.length; j++) {
-				if (iteratedList.get(cardIndex).getCardValue() == values[j]
-						&& iteratedList.get(cardIndex).getColor() == colors[i]) {
+		for (int i = 0; i < Card.Color.values().length; i++) {
+			for (int j = 0; j < Card.CardValue.values().length; j++) {
+				if (differentCardsStub.get(cardIndex).getCardValue() == values[j]
+						&& differentCardsStub.get(cardIndex).getColor() == colors[i]) {
+					actual++;
+					cardIndex++;
+				}
+			}
+		}
+		assertEquals(expected, actual);
+
+	}
+	@Test
+	public void Deck_DontCountSameCard_Return1() {
+
+		int actual = 0;
+		int expected = 1;
+		int cardIndex = 0;
+		ArrayList<CardStub> sameCardsStub = sameCardsStub();
+		Card.Color[] colors = Card.Color.values();
+		Card.CardValue[] values = Card.CardValue.values();
+		for (int i = 0; i < Card.Color.values().length; i++) {
+			for (int j = 0; j < Card.CardValue.values().length; j++) {
+				if (sameCardsStub.get(cardIndex).getCardValue() == values[j]
+						&& sameCardsStub.get(cardIndex).getColor() == colors[i]) {
 					actual++;
 					cardIndex++;
 				}
@@ -49,6 +71,8 @@ class DeckTests {
 
 	}
 
+	
+	
 	@Test
 	public void DrawCard_CheckDeckSizeDecrease_Return51() {
 		sut.drawCard();
@@ -61,7 +85,7 @@ class DeckTests {
 	public void DrawCard_OnEmptyDeck_ReturnException() {
 		boolean thrown = false;
 		try {
-			int size = iteratedList.size();
+			int size = sut.getSize();
 			for (int i = 0; i < size + 1; i++) {
 				sut.drawCard();
 			}
@@ -71,5 +95,49 @@ class DeckTests {
 		assertTrue(thrown);
 	}
 
-}
+	private class CardStub extends Card {
 
+		public CardStub(Color color, CardValue cardValue) {
+			super(color, cardValue);
+		}
+
+	}
+
+	private ArrayList<CardStub> differentCardsStub() {
+		ArrayList<CardStub> differentCardsStub = new ArrayList<CardStub>();
+
+
+		for (int i = 0; i < Card.Color.values().length; i++) {
+
+			for (int j = 0; j < Card.CardValue.values().length; j++) {
+				CardStub c = new CardStub(Card.Color.values()[i], Card.CardValue.values()[j]);
+
+				differentCardsStub.add(c);
+
+			}
+		}
+
+		return differentCardsStub;
+
+	}
+	private ArrayList<CardStub> sameCardsStub() {
+		ArrayList<CardStub> differentCardsStub = new ArrayList<CardStub>();
+
+
+		for (int i = 0; i < Card.Color.values().length; i++) {
+
+			for (int j = 0; j < Card.CardValue.values().length; j++) {
+				CardStub c = new CardStub(Card.Color.values()[0], Card.CardValue.values()[0]);
+
+				differentCardsStub.add(c);
+
+			}
+		}
+
+		return differentCardsStub;
+
+	}
+	
+	
+
+}
