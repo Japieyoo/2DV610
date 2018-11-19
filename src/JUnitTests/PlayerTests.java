6fdf8cb38b46a2ebe9944c89b4ea8b0c07815a9d
Japieyoo <japieyoo@gmail.com>
2018-11-19 15:59:05 +0100

    REFACTOR

--- src/JUnitTests/PlayerTests.java
+++ src/JUnitTests/PlayerTests.java
@@ -2,14 +2,21 @@ package JUnitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import model.Player;
 
 class PlayerTests {
+	Player p;
+
+	@BeforeEach
+	public void setUp() {
+		p = new Player();
+	}
 
 	@Test
 	public void setName_ShouldSetANameForThePlayer() {
-		Player p = new Player();
+
 		p.setName("Test");
 		String actual = p.name;
 		String expected = "Test";
